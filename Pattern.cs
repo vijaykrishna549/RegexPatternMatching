a5ee3670a2a022801f3e27607f4b0d4a03d836bc
pvijaykrishna549 <you@example.com>
2021-04-22 14:41:47 +0530

    RegexPatternMatching_With_all_UseCases_Commented_Out

---
+++ Pattern.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexPatternMatchingDemo
+{
+    class Pattern
+    {
+        //aaaaaa, sdfg45// pincode -> 6digi , 234 678   345654 [ ]
+        //string Regex_Pincode="^[1-9][0-9]{2}[ ]?[0-9]{3}$";
+        // string Regex_Pincode = "^[a-z0-9A-Z]+[@][a-z0-9A-Z]+[.][a-zA-Z]{2,3}$";
+        //string Regex_Pincode = "^[a-z0-9A-Z]+([._+-][a-z0-9A-Z]+)*[@][a-z0-9A-Z]+[.][a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
+        // string Regex_Pincode = " ^[0-9]{0,}[a-zA-Z]{3,}[a-zA-Z0-9]*$";
+        // string Regex_Pincode = "^[1-9][0-9]{5}$";
+        string Regex_Pincode = "^[1-9][0-9]{2}[ ]?[0-9]{3}$";
+        public bool validatePinCode(string pincode)
+        {
+            return Regex.IsMatch(pincode, Regex_Pincode);
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RegexPatternMatchingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Pattern pattern = new Pattern();
+                //bool result=pattern.validatePinCode("abc-100-100@.nt.in");
+                //bool result = pattern.validatePinCode("400 088");
+                bool result = pattern.validatePinCode("841 423");
+                Console.WriteLine(result);
+                Console.ReadKey();
+            }
+
+
+        }
+    }
+}
